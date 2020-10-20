13df9a1e50411e85cb304b42a5a5ebe53eb4b365
anhp58 <anhphan.uet@gmail.com>
2020-10-20 16:05:11 +0700

    update comment for app.py

--- app.py
+++ app.py
@@ -12,20 +12,23 @@ import dash_html_components as html
 from init import *
 from const import *
 
-
+# generate Vietnam's Figure and Phil's figure from init.py
 PHILfig, VNfig = genearateFigure(PHIL_HAZARD, PHIL_POLICY, VN_HAZARD, VN_POLICY)
 
 #dash init
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app = dash.Dash(title='GCRF Visualization')
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css'] # ref to css style
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets) # init dash component
+app = dash.Dash(title='GCRF Visualization') # set title
 server = app.server # get server variable, this varibale needs to be global varible
 app.layout = html.Div(children=[
     html.H1(children='GCRF - Chronological Figure'),
+    # add Phil's graph
     dcc.Graph(
         id='P',
         figure=PHILfig
     ),
+    # add Vietnamese's graph
     dcc.Graph(
         id='VN',
         figure=VNfig
